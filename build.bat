c72a3091d972d0d8eea36e34362b92767e74d5e0
Richard Sim <rsim@devras.com>
2023-01-23 16:24:31 -0800

    Fixed up HLA path in Release builds

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+mkdir build
+cd build
+
+REM NOTE: Windows requires `--config Release` at build time, whereas MacOS and Linux use `-DCMAKE_BUILD_TYPE=Release` at configure time
+
+REM Configure debug (any configuration on Windows)
+"C:\Program Files\CMake\bin\cmake.exe" .. -A x64
+
+REM Configure release
+REM "C:\Program Files\CMake\bin\cmake.exe" .. -A x64 -DCMAKE_BUILD_TYPE=Release
+
+REM Build debug (configured configuration on MacOS and Linux)
+"C:\Program Files\CMake\bin\cmake.exe" --build .
+
+REM Build release
+REM "C:\Program Files\CMake\bin\cmake.exe" --build . --config Release
